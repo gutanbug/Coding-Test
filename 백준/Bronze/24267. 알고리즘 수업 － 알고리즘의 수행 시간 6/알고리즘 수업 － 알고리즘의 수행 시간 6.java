c2b8f0aa218be38575d7af217444b8ada044e23c
ChaHyeonMin <112674303+gutanbug@users.noreply.github.com>
2024-03-19 00:10:18 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 212 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long sum = 0;
+
+        for(; n - 2 > 0; n--) {
+            sum += (n-2)*(n-1) / 2;
+        }
+        System.out.println(sum);
+        System.out.println(3);
+    }
+}
