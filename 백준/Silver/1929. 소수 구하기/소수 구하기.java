055b460e47127a5ed693b2515187c1c0a77d812f
ChaHyeonMin <112674303+gutanbug@users.noreply.github.com>
2024-03-29 22:20:28 +0900

    [Silver III] Title: 소수 구하기, Time: 800 ms, Memory: 29868 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static boolean[] prime;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        prime = new boolean[N+1];
+        get_prime();
+
+        for(int i = M; i <= N; i++) {
+            if(!prime[i]) System.out.println(i);
+        }
+    }
+
+    public static void get_prime() {
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+}
