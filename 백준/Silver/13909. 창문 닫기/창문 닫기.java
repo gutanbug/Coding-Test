c3e533fe8cee9133d597725be594bcefc7806d46
ChaHyeonMin <112674303+gutanbug@users.noreply.github.com>
2024-04-01 11:05:57 +0900

    [Silver V] Title: 창문 닫기, Time: 120 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for(int i = 1; i * i <= N; i++) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
